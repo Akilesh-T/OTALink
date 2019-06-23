f01c08e27bb4df4937a53d1e235e99ef176fc01a
Akilesh <akilesh.t.15@gmail.com>
2019-06-23 12:07:06 +0530

    Minor changes.
    Handle a case when device is not rooted.

--- app/src/main/java/app/akilesh/ota/MainActivity.kt
+++ app/src/main/java/app/akilesh/ota/MainActivity.kt
@@ -16,32 +16,8 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import androidx.core.app.NotificationCompat
-*/
 
-
-class MainActivity : AppCompatActivity() {
-    private var tag: String = "OTA"
-    private lateinit var textView: TextView
-    private lateinit var url: String
-    private lateinit var cmd: String
-
-    @SuppressLint("SdCardPath")
-    private var dataFile = "/data/data/com.google.android.gms/shared_prefs/com.google.android.gms.update.storage.xml"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val decorView = window.decorView
-        decorView.systemUiVisibility = FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS or SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-
-        if(File(dataFile).exists()) {
-            cmd = "cat $dataFile"
-            val msg = "Reading from $dataFile"
-            Log.d(tag, msg)
-
-
-           /* val intent = Intent(this, MainActivity::class.java).apply {
+            val intent = Intent(this, MainActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             }
             val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
@@ -62,16 +38,38 @@ class MainActivity : AppCompatActivity() {
             val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(mChannel)
             notificationManager.notify(1, builder.build())
+*/
 
-            // Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-            /*
+@SuppressLint("SdCardPath")
+class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var url: String
+    private lateinit var cmd: String
+
+    private var dataFile = "/data/data/com.google.android.gms/shared_prefs/com.google.android.gms.update.storage.xml"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val decorView = window.decorView
+        decorView.systemUiVisibility = FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS or SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+
+        textView = findViewById(R.id.url)
+
+        if(File(dataFile).exists()) {
+            cmd = "cat $dataFile"
+            val msg = "Reading from $dataFile"
+            Log.d(TAG, msg)
+
+            /* Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
         else {
             cmd = "logcat | grep packages/ota"
             val msg = "Reading from logcat"
             Log.d(tag, msg)
-           // Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
         }
-        */
         */
 
             val process = Runtime.getRuntime().exec("su")
@@ -83,26 +81,31 @@ class MainActivity : AppCompatActivity() {
             process.waitFor()
 
             if (process.exitValue() != 0) {
-                Log.e(tag, "Failed to obtain root")
-            } else {
+                Log.e(TAG, "Failed to obtain root")
+                textView.text = String.format("%s", "Unable to get root access. Take a bug report or use adb logcat to get the ota link.")
+            }
+            else {
                 var ch: Int
                 val sb = StringBuilder()
                 while (`in`.read().let { ch = it; it != -1 })
                     sb.append(ch.toChar())
                 url = sb.toString().trim()
+
+                val regex = "(https)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*(zip)".toRegex()
+                val matchResult = regex.find(url)
+                val result = matchResult?.value
+
+                if (matchResult != null) {
+                    textView.setTextIsSelectable(true)
+                    textView.text = String.format("%s", result)
+                } else
+                    textView.text = String.format("%s", "No updates available.")
             }
-
-            val regex = "(https)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*(zip)".toRegex()
-            val matchResult = regex.find(url)
-
-            textView = findViewById(R.id.url)
-            textView.setTextIsSelectable(true)
-            textView.text = String.format("%s", matchResult?.value)
-
         }
-        else{
-            textView = findViewById(R.id.url)
-            textView.text = String.format("%s", "No updates available.")
-        }
+    }
+
+    companion object{
+        internal const val TAG = "MainActivity"
+
     }
 }
